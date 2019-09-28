2d0380f07bbdf0410fffbfc0bd2330478ad477bf
Andrey Sheptikin <sheptikin@xrm.ru>
2019-09-28 15:26:10 +0500

    add new components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CarComponent } from './car/car.component';
+import { CarsComponent } from './cars/cars.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CarComponent,
+    CarsComponent
   ],
   imports: [
     BrowserModule,
