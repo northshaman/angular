99781326c7d26b93b31f3fb614763aa7414fcd7a
Andrey Sheptikin <sheptikin@xrm.ru>
2019-09-29 12:52:16 +0500

    add delete button

--- src/app/car/car.component.ts
+++ src/app/car/car.component.ts
@@ -1,11 +1,21 @@
-import {Component, Input} from '@angular/core';
+import {Component, DoCheck, Input, OnChanges, SimpleChanges} from '@angular/core';
 
 @Component({
   selector: 'app-car',
   templateUrl: './car.component.html',
   styleUrls: ['./car.component.css']
 })
-export class CarComponent {
+export class CarComponent implements OnChanges, DoCheck{
   @Input('carItem') car: { name: string, year: number };
+  @Input('name') carName: string;
   private carItem = false;
+
+  ngOnChanges(changes: SimpleChanges) {
+    console.log('OnChanges', changes);
+  }
+
+  ngDoCheck() {
+    console.log('doCheck');
+  }
+
 }
--- src/app/cars/cars.component.ts
+++ src/app/cars/cars.component.ts
@@ -1,32 +1,45 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-cars',
   templateUrl: './cars.component.html',
   styleUrls: ['./cars.component.css']
 })
-export class CarsComponent {
+export class CarsComponent implements OnInit {
 
   // @ts-ignore
-  cars: [{name: string , year: number}] = [{
+  cars: [{ name: string, year: number }] = [{
     name: 'Ford',
     year: 2017
-  }, {
-      name: 'Mazda',
-      year: 2015
-  }, {
-    name: 'Audi',
-    year: 2010
-  }, {
-    name: 'Ford',
-    year: 2017
+    // }, {
+    //     name: 'Mazda',
+    //     year: 2015
+    // }, {
+    //   name: 'Audi',
+    //   year: 2010
+    // }, {
+    //   name: 'Ford',
+    //   year: 2017
   }];
 
 
   constructor() {
+    console.log('contstrucor cars');
   }
 
-  updateCarList(car: {name: string, year: number}) {
+  updateCarList(car: { name: string, year: number }) {
     this.cars.push(car);
+  }
+
+  changeCarName() {
+    this.cars[0].name = 'New Car Name';
+  }
+
+  ngOnInit() {
+    console.log('onInit');
+  }
+
+  deleteCar() {
+    this.cars.splice(0, 1);
   }
 }
