32393653032523327f61e57c53b43fd67147b985
Andrey Sheptikin <sheptikin@xrm.ru>
2019-09-29 13:18:03 +0500

    add new directives

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,17 @@ import { CarComponent } from './car/car.component';
 import { CarsComponent } from './cars/cars.component';
 import {FormsModule} from '@angular/forms';
 import { AddCarComponent } from './add-car/add-car.component';
+import {BackgroundDirective} from './directives/background.directive';
+import { MyDirectiveDirective } from './directives/my-directive.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     CarComponent,
     CarsComponent,
-    AddCarComponent
+    AddCarComponent,
+    BackgroundDirective,
+    MyDirectiveDirective
   ],
   imports: [
     BrowserModule,
---
+++ src/app/directives/background.directive.ts
@@ -0,0 +1,13 @@
+import {Directive, ElementRef, OnInit} from '@angular/core';
+
+@Directive({
+  selector: '[appBackground]'
+})
+export class BackgroundDirective implements OnInit{
+  constructor(private element: ElementRef) {}
+
+  ngOnInit(){
+    console.log(this.element);
+    this.element.nativeElement.style.backgroundColor = 'red';
+  }
+}
